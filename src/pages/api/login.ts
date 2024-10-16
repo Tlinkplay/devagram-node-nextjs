a16391ca6c0c8f97026788b343bb5f06c185c93a
Link <djoao.pedro2012@gmail.com>
2024-10-16 00:15:37 -0300

    configiração do banco de dados

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,16 +1,18 @@
 import { error } from "console";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { conectarMongoDB } from '../../../middlewares/conectarMongoDB';
+import type {RespostaPadraoMsg} from '../../../types/RespostaPadraoMSG';
 
-export default (
+const endpointLogin = (
     req:NextApiRequest,
-    res:NextApiResponse
+    res:NextApiResponse<RespostaPadraoMsg>
 ) => {
     if(req.method === 'POST'){
         const {login,senha} = req.body;
 
         if(login === 'admin@admin.com' &&
             senha === 'Admin@123'){
-                res.status(200).json({msg : 'Usuario autenticado com sucesso'})
+                return res.status(200).json({msg : 'Usuario autenticado com sucesso'})
 
             }
             return res.status(400).json({erro : 'Usuario ou senha não encontrado'});
@@ -18,3 +20,5 @@ export default (
     }
     return res.status(405).json({erro: 'Metodo informado não é valido'});
 }
+
+export default conectarMongoDB(endpointLogin);
