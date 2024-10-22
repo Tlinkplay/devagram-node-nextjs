e4450ef6203a898d1ba8544bd99aec8da74a2d12
Link <djoao.pedro2012@gmail.com>
2024-10-22 19:42:26 -0300

    Like e Deslike

--- src/pages/api/pesquisa.ts
+++ src/pages/api/pesquisa.ts
@@ -8,20 +8,28 @@ const pesquisaEndPoint = async(req : NextApiRequest, res : NextApiResponse<Respo
 
     try{
         if(req.method === 'GET'){
+            if(req?.query?.id){
+                const usuarioEncontrado = await UsuarioModel.findById(req?.query?.id);
+                if(!usuarioEncontrado){
+                    return res.status(400).json({erro: 'Usuario não encontrado'})
+                }
+                usuarioEncontrado.senha = null;
+                return res.status(200).json(usuarioEncontrado);
+            }else{
+                const {filtro} = req.query;
 
-            const {filtro} = req.query;
-
-            if(!filtro || filtro.length < 2){
-                return res.status(400).json({erro : 'Favor informar pelo menos 2 caracteres para a busca'});
-            }
-
-            const usuariosEncontrados = await UsuarioModel.find({
-                $or: [{nome : {$regex : filtro, $options : 'i'}}
-                    //{email : {$regex : filtro, $options: 'i'}}
-                ]
-            });
-
-            return res.status(200).json(usuariosEncontrados);
+                if(!filtro || filtro.length < 2){
+                    return res.status(400).json({erro : 'Favor informar pelo menos 2 caracteres para a busca'});
+                }
+    
+                const usuariosEncontrados = await UsuarioModel.find({
+                    $or: [{nome : {$regex : filtro, $options : 'i'}}
+                        //{email : {$regex : filtro, $options: 'i'}}
+                    ]
+                });
+    
+                return res.status(200).json(usuariosEncontrados);
+            }    
         }
         return res.status(405).json({erro : 'Metodo informado não é valido'});
     }catch(e){
